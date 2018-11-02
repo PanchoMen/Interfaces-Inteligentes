e06e5bd278a20ee6f22a29d4b7848f6853774f1f
PanchoMen <alu0100846768@ull.edu.es>
2018-11-02 00:39:13 +0000

    Creado el laberinto, añadido un personaje con linterna

---
+++ Practica 3/Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameController : MonoBehaviour {
+
+    public static GameController controlador;
+
+    delegate void Controller();
+    Controller controller;
+
+    private void Awake() {
+        if(controlador == null) {
+            controlador = this;
+            DontDestroyOnLoad(this);
+        } else if(controlador != this) {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Practica 3/Assets/Scripts/Linterna.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Linterna : MonoBehaviour {
+
+    Light linterna;
+
+    // Use this for initialization
+    void Start () {
+        linterna = gameObject.GetComponent<Light>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyUp(KeyCode.L)){
+            OnOff();
+        }
+	}
+
+    void OnOff(){
+        if (linterna.enabled == true) {
+            linterna.enabled = false;
+        } else {
+            linterna.enabled = true;
+        }
+    }
+}
